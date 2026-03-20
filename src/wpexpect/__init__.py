3441073e9d825b7fbfa98c57cae85913759220c4
Jasper Chan <jasperchan515@gmail.com>
2026-03-19 18:37:48 -0700

    bugfix: don't import unix only funcs from pexpect on win32

--- src/wpexpect/__init__.py
+++ src/wpexpect/__init__.py
@@ -1,6 +1,17 @@
 import sys
 
-from pexpect import *  # noqa: F403
 
 if sys.platform == "win32":
     from wexpect import run, spawn  # noqa: F401
+    import pexpect
+
+    globals().update(
+        {
+            name: getattr(pexpect, name)
+            for name in dir(pexpect)
+            if not name.startswith("_")
+            and name not in ("spawn", "run", "spawnu", "runu")
+        }
+    )
+else:
+    from pexpect import *
