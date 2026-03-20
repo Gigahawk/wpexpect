91cb94805f96640bf300b86d452b78625c1d897c
Jasper Chan <jasperchan515@gmail.com>
2026-03-19 18:11:43 -0700

    chore: fix lint errors

--- src/wpexpect/__init__.py
+++ src/wpexpect/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-from pexpect import *
+from pexpect import *  # noqa: F403
 
 if sys.platform == "win32":
-    from wexpect import run, spawn
+    from wexpect import run, spawn  # noqa: F401
